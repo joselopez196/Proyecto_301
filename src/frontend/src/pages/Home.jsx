8db75d195f7a44a22ddbed96c7a262c905249a1d
joselopez196 <joseoctaviolopezrios196@gmail.com>
2025-10-29 11:08:11 -0500

    feat:creando configuracion inicial de React

---
+++ src/frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="min-h-screen bg-gray-100 p-8">
+            <h1 className="text-2xl font-bold mb-4">Bienvenido a mi pagina</h1>
+            <p className="text-orange-600">Esta es una pagina utilizando Tailwind CSS.</p>
+            </div>
+    );
+}
+
+export default Home;
